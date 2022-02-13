ba9764766924eb618c01ab698539bb868b1cda24
Jun ASAKA <Jun@zzy040330.cn>
2022-02-13 12:32:20 +0800

    Version 3.1 supports both nonSAR and SAR

--- modify_phh_gsi.sh
+++ modify_phh_gsi.sh
@@ -1,12 +1,9 @@
 # !/bin/sh
 
 #  author: Yahoo Mike @ xda-developers
-#    date: 4 September 2021
-# version: 2.0
-
-#  modified to support System-As-Root by Jun ASAKA
-#  Only use to patch System-As-Root ROMs!
-#  In the non System-As-Root situation, use the one unmodified: https://forum.xda-developers.com/t/project-treble-gsi-images-on-smart-tab-m10-tb-x605f-l-and-p10-tb-x705f-l.4147201/.
+#          Jun ASAKA @ xda-developers
+#    date: 13 February 2022
+# version: 3.1
 
 usage () {
 	echo " "
@@ -37,8 +34,6 @@ usage () {
 	echo "flashed a phhusson-based GSI ROM to the /system partition already."
 	echo " "
 	echo "This script is only supported for Lenovo TB-X605F/L and TB-X705F/L."
-	echo " "
-	echo "Modified by Jun ASAKA to use when System-As-Root"
 	echo " "
 }
 
@@ -122,29 +117,52 @@ if [[ $(getprop ro.twrp.boot) != *"1"* ]]; then
     exit 1
 fi
 
-# 3. mount /system rw
+# 3. check if device has been re-configured as system-as-root (SAR)
+_SAR=0
+if [[ $(getprop ro.twrp.sar) == "true" ]]; then
+    # device modified to be SAR (using MakeMeSAR or similar)
+    # see: https://forum.xda-developers.com/t/android-s-gsi-system-as-root-on-on-smart-tab-m10-tb-x605f.4400689/
+    _SAR=1
+    echo "System As Root detected"
+fi
 
-#Modified Here By Jun ASAKA:
-
-#umount /system 2>/dev/null
-#mount -o rw /system
-#rc=$?
-#if [[ $rc -ne 0 ]]; then
-#    echo "ERROR: failed to mount /system read-write with rc=$rc"
-#    exit 1
-#fi
-
+# 4. check integrity and resize fs on /system
 umount /system 2>/dev/null
 umount /system_root 2>/dev/null
-mount -o rw /dev/block/bootdevice/by-name/system /system_root/
-mount -o bind /system_root/system /system
+
+# some devices require an integrity check before resizing...
+echo "Running e2fsck..."
+e2fsck -fp /dev/block/bootdevice/by-name/system
 rc=$?
+if [[ $rc -ne 0 ]]; then
+    echo "ERROR: failed to check integrity of system filesystem with rc=$rc"
+    exit 1
+fi
+
+echo "Running resize2fs..."
+resize2fs /dev/block/bootdevice/by-name/system
+rc=$?
+if [[ $rc -ne 0 ]]; then
+    echo "ERROR: failed to resize system filesystem to partition size with rc=$rc"
+    exit 1
+fi
+
+# 5. mount /system (and /system_root for SAR) rw
+if [ $_SAR -eq 0 ]; then
+    # stock non-SAR device
+    mount -o rw /system
+    rc=$?
+else
+    mount -o rw /dev/block/bootdevice/by-name/system /system_root/
+    mount -o bind /system_root/system /system
+    rc=$?
+fi
 if [[ $rc -ne 0 ]]; then
     echo "ERROR: failed to mount /system read-write with rc=$rc"
     exit 1
 fi
 
-# 4. check that /system/bin/rw-system.sh exists
+# 6. check that /system/bin/rw-system.sh exists
 if [ ! -f "/system/bin/rw-system.sh" ]; then
     echo "ERROR: /system/bin/rw-system.sh does not exist.  Is this a phhusson-based GSI ROM?"
     exit 1
@@ -152,16 +170,7 @@ fi
 
 echo "System mounted and all initial checks passed..."
 
-# 5. resize2fs  (run this before modifying rw-system.sh, so we don't run out of space)
-echo "Running resize2fs..."
-resize2fs "$(grep ' /system ' /proc/mounts | cut -d ' ' -f 1)"
-rc=$?
-if [[ rc -ne 0 ]]; then
-    echo "ERROR: failed to resize system filesystem to partition size with rc=$rc"
-    exit 1
-fi
-
-# 6. read rw-system.sh and comment out the offending lines
+# 7. read rw-system.sh and comment out the offending lines
 echo "Modifying rw-system.sh..."
 modify_file
 rc=$?
@@ -170,17 +179,18 @@ if [[ $rc -ne 0 ]]; then
     exit 1
 fi
 
-# 7. for Android 11 GSIs, correct the mount binding of libstagefright_foundation.so
+# 8. for Android 11+ GSIs, correct the mount binding of libstagefright_foundation.so
 #    For more info see: https://github.com/phhusson/treble_experimentations/issues/1917
 #    NOTE: this will have no effect on Android 10 GSIs.
-echo "Correcting mount bindings for Android 11 GSIs..."
+echo "Correcting mount bindings for Android 11+ GSIs..."
 sed -i 's|/system/system_ext/apex/|/apex/|' /system/bin/rw-system.sh
 	
 chmod 755 /system/bin/rw-system.sh
 chcon u:object_r:phhsu_exec:s0 /system/bin/rw-system.sh
 
-# 8. unmount /system
+# 9. unmount /system (and /system_root for SAR)
 umount /system 2>/dev/null
+umount /system_root 2>/dev/null
 
 echo " "
 echo "...finished."
